8dcd48b91391e787ab5b2f45a270449924615897
deadcast <deadcast@sent.com>
2023-07-01 19:39:39 -0500

    Removed banner from chrome

--- ConsoleApp/App.py
+++ ConsoleApp/App.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
+from time import sleep
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions(); 
+
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation']);
+
+driver = webdriver.Chrome(options=chrome_options)
+
 driver.get('http://localhost:5000');
+
 driver.set_window_size(800, 480)
+
 driver.save_screenshot("screenshot.png")
+
 driver.quit()
